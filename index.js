53d04b83dd4d089614336785e5c3d43a6cb5a204
Dan Bowen <danbowen2677@gmail.com>
2023-03-29 11:52:30 -0400

    wrote questions for the various types of inputs needed from users

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 // Add in necessary packages
-// inquirer
+const inquirer = require('inquirer');
 
 // Create on array of questions that the user can answer via the command line
 // Questions to ask include: text, color, and shape input, as well as shape color.
+const questions = [
+    {
+      type: 'input',
+      name: 'logo-text',
+      message: 'Select characters for your logo. Enter up to three characters: ',
+    },
+    {
+      type: 'checkbox',
+      message: 'Select a shape for your logo',
+      name: 'shape-type',
+      choices: ['Circle','Square','Triangle'],
+    },
+    {
+        type: 'input',
+        name: 'logo-color',
+        message: 'Select a color for the shape of your logo. Enter a color keyword (or a hexadecimal number):',
+      },
+      {
+        type: 'input',
+        name: 'text-color',
+        message: 'Select a color for the text of your logo. Enter a color keyword (or a hexadecimal number):',
+      },
+  ]
 
 // Create a constructor function for the logo
 
